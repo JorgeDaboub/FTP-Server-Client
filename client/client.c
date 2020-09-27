fc1c5d3934497898e8eeec3fa0933fc481a1c9d9
jdaboub <jdaboub@gitlab.com>
2020-09-27 18:15:28 -0400

    Progress

--- client/client.c
+++ client/client.c
@@ -1,141 +1,186 @@
 
 /*FTP Client*/
- 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
+
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
 /*for getting file size using stat()*/
-#include<sys/stat.h>
- 
+#include <sys/stat.h>
+
 /*for sendfile()*/
-#include<sys/sendfile.h>
- 
+#include <sys/sendfile.h>
+
 /*for O_RDONLY*/
-#include<fcntl.h>
- 
-int main(int argc,char *argv[])
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
 {
-  struct sockaddr_in server;
-  struct stat obj;
-  int sock;
-  int choice;
-  char buf[100], command[5], filename[20], *f;
-  int k, size, status;
-  int filehandle;
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if(sock == -1)
-    {
-      printf("socket creation failed");
-      exit(1);
-    }
-  server.sin_family = AF_INET;
-  server.sin_port = atoi(argv[1]);
-  server.sin_addr.s_addr = 0;
-  k = connect(sock,(struct sockaddr*)&server, sizeof(server));
-  if(k == -1)
-    {
-      printf("Connect Error");
-      exit(1);
-    }
-  int i = 1;
-  while(1)
-    {
-      printf("Enter a choice:\n1- get\n2- put\n3- pwd\n4- ls\n5- cd\n6- quit\n");
-      scanf("%d", &choice);
-      switch(choice)
+	struct sockaddr_in sin;
+	struct stat obj;
+	struct hostent *hp;
+	char *host;
+	int SERVER_PORT;
+	int sock;
+	int choice;
+	char buf[BUFSIZ], command[5], filename[20], *f;
+	int k, size, status;
+	int len;
+	int filehandle;
+	int file_size;
+	FILE *received_file;
+	int remain_data = 0;
+
+	// Parse Command Line Arguments
+	if (argc == 3)
 	{
-	case 1:
-	  printf("Enter filename to get: ");
-	  scanf("%s", filename);
-	  strcpy(buf, "get ");
-	  strcat(buf, filename);
-	  send(sock, buf, 100, 0);
-	  recv(sock, &size, sizeof(int), 0);
-	  if(!size)
-	    {
-	      printf("No such file on the remote directory\n\n");
-	    break;
-	    }
-	  f = malloc(size);
-	  recv(sock, f, size, 0);
-	  while(1)
-	    {
-	      filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
-	      if(filehandle == -1)
-		{
-		  sprintf(filename + strlen(filename), "%d", i);//needed only if same directory is used for both server and client
-		}
-	      else break;
-	    }
-	  write(filehandle, f, size, 0);
-	  close(filehandle);
-	  strcpy(buf, "cat ");
-	  strcat(buf, filename);
-	  system(buf);
-	  break;
-	case 2:
-	  printf("Enter filename to put to server: ");
-          scanf("%s", filename);
-	  filehandle = open(filename, O_RDONLY);
-          if(filehandle == -1)
-            {
-              printf("No such file on the local directory\n\n");
-              break;
-            }
-          strcpy(buf, "put ");
-	  strcat(buf, filename);
-	  send(sock, buf, 100, 0);
-	  stat(filename, &obj);
-	  size = obj.st_size;
-	  send(sock, &size, sizeof(int), 0);
-	  sendfile(sock, filehandle, NULL, size);
-	  recv(sock, &status, sizeof(int), 0);
-	  if(status)
-	    printf("File stored successfully\n");
-	  else
-	    printf("File failed to be stored to remote machine\n");
-	  break;
-	case 3:
-	  strcpy(buf, "pwd");
-	  send(sock, buf, 100, 0);
-	  recv(sock, buf, 100, 0);
-	  printf("The path of the remote directory is: %s\n", buf);
-	  break;
-	case 4:
-	  strcpy(buf, "ls");
-          send(sock, buf, 100, 0);
-	  recv(sock, &size, sizeof(int), 0);
-          f = malloc(size);
-          recv(sock, f, size, 0);
-	  filehandle = creat("temp.txt", O_WRONLY);
-	  write(filehandle, f, size, 0);
-	  close(filehandle);
-          printf("The remote directory listing is as follows:\n");
-	  system("cat temp.txt");
-	  break;
-	case 5:
-	  strcpy(buf, "cd ");
-	  printf("Enter the path to change the remote directory: ");
-	  scanf("%s", buf + 3);
-          send(sock, buf, 100, 0);
-	  recv(sock, &status, sizeof(int), 0);
-          if(status)
-            printf("Remote directory successfully changed\n");
-          else
-            printf("Remote directory failed to change\n");
-          break;
-	case 6:
-	  strcpy(buf, "quit");
-          send(sock, buf, 100, 0);
-          recv(sock, &status, 100, 0);
-	  if(status)
-	    {
-	      printf("Server closed\nQuitting..\n");
-	      exit(0);
-	    }
-	    printf("Server failed to close connection\n");
+		host = argv[1];
+		SERVER_PORT = atoi(argv[2]);
 	}
-    }
+	else
+	{
+		fprintf(stderr, "usage: client host port\n");
+		exit(1);
+	}
+
+	/* translate host name into peer's IP address */
+	hp = gethostbyname(host);
+	if (!hp)
+	{
+		fprintf(stderr, "client: unknown host: %s\n", host);
+		exit(1);
+	}
+
+	/* build address data structure */
+	bzero((char *)&sin, sizeof(sin));
+	sin.sin_family = AF_INET;
+	bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+	sin.sin_port = htons(SERVER_PORT);
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+	{
+		printf("socket creation failed");
+		exit(1);
+	}
+
+	k = connect(sock, (struct sockaddr *)&sin, sizeof(sin));
+	if (k == -1)
+	{
+		printf("Connect Error");
+		exit(1);
+	}
+	int i = 1;
+
+	while (1)
+	{
+		printf("Enter a choice:\n1- get\n2- put\n3- pwd\n4- ls\n5- cd\n6- quit\n");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			printf("Enter filename to get: ");
+			scanf("%s", filename);
+			strcpy(buf, "get ");
+			strcat(buf, filename);
+			printf("Sending TO SERVER %s\n", buf);
+			send(sock, buf, 100, 0);
+
+			bzero(buf, sizeof(buf));
+			len = recv(sock, buf, BUFSIZ, 0);
+			buf[len] = '\0';
+			file_size = 19;
+			printf("Received Size %d\n", file_size);
+
+			received_file = fopen(filename, "w");
+			if (received_file == NULL)
+			{
+				printf("Failed to open file foo --> \n");
+
+				exit(EXIT_FAILURE);
+			}
+
+			remain_data = file_size;
+			while ((remain_data > 0) && ((len = recv(sock, buf, BUFSIZ, 0)) > 0))
+			{
+				printf("%s", buf);
+				fwrite(buf, sizeof(char), len, received_file);
+				remain_data -= len;
+				printf("Receive %d bytes and we hope :- %d bytes\n", len, remain_data);
+			};
+			close(received_file);
+			strcpy(buf, "cat ");
+			strcat(buf, filename);
+			system(buf);
+			break;
+			
+		case 2:
+			printf("Enter filename to put to server: ");
+			scanf("%s", filename);
+			filehandle = open(filename, O_RDONLY);
+			if (filehandle == -1)
+			{
+				printf("No such file on the local directory\n\n");
+				break;
+			}
+			strcpy(buf, "put ");
+			strcat(buf, filename);
+			send(sock, buf, 100, 0);
+			stat(filename, &obj);
+			size = obj.st_size;
+			send(sock, &size, sizeof(int), 0);
+			sendfile(sock, filehandle, NULL, size);
+			recv(sock, &status, sizeof(int), 0);
+			if (status)
+				printf("File stored successfully\n");
+			else
+				printf("File failed to be stored to remote machine\n");
+			break;
+		case 3:
+			strcpy(buf, "pwd");
+			send(sock, buf, 100, 0);
+			recv(sock, buf, 100, 0);
+			printf("The path of the remote directory is: %s\n", buf);
+			break;
+		case 4:
+			strcpy(buf, "ls");
+			send(sock, buf, 100, 0);
+			recv(sock, &size, sizeof(int), 0);
+			f = malloc(size);
+			recv(sock, f, size, 0);
+			filehandle = creat("temp.txt", O_WRONLY);
+			write(filehandle, f, size, 0);
+			close(filehandle);
+			printf("The remote directory listing is as follows:\n");
+			system("chmod +rw temp.txt");
+			system("cat temp.txt");
+			break;
+		case 5:
+			strcpy(buf, "cd ");
+			printf("Enter the path to change the remote directory: ");
+			scanf("%s", buf + 3);
+			send(sock, buf, 100, 0);
+			recv(sock, &status, sizeof(int), 0);
+			if (status)
+				printf("Remote directory successfully changed\n");
+			else
+				printf("Remote directory failed to change\n");
+			break;
+		case 6:
+			strcpy(buf, "quit");
+			send(sock, buf, 100, 0);
+			recv(sock, &status, 100, 0);
+			if (status)
+			{
+				printf("Server closed\nQuitting..\n");
+				exit(0);
+			}
+			printf("Server failed to close connection\n");
+		}
+	}
+	return 0;
+}
--- makefile
+++ makefile
@@ -1,17 +1,18 @@
 all: bin/ftp
 
-bin/client.o: client/client.c
+bin/client/client.o: client/client.c
 	gcc -g -Wall -Werror -std=c99 -Iinclude client/client.c -c -o bin/client.o
 
-bin/server.o: server/server.c
+bin/server/server.o: server/server.c
 	gcc -g -Wall -Werror -std=c99 -Iinclude server/server.c -c -o bin/server.o
 
 lib/ftp.a:
 	ar rcs lib/ftp.a
 
 bin/ftp: lib/ftp.a server/server.c client/client.c
-	gcc -L lib/ftp.a server/server.c -o bin/server
-	gcc -L lib/ftp.a client/client.c -o bin/client
+	gcc -L lib/ftp.a server/server.c -o bin/server/server.o
+	gcc -L lib/ftp.a client/client.c -o bin/client/client.o
 
 clean:
-	rm bin/*
+	rm bin/server/*.o
+	rm bin/client/*.o
